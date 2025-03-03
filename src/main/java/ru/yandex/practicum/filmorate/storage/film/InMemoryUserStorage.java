e65ff2713496a8730a8972210337d5918f22d2e6
Gosling <popkornuh@gmail.com>
2025-03-03 22:51:38 +0300

    ФЗ 11 спринта

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryUserStorage.java
@@ -10,7 +10,7 @@ import java.util.*;
 
 @Slf4j
 @Component
-public class InMemoryUserStorage implements UserStorage{
+public class InMemoryUserStorage implements UserStorage {
 
     private final Map<Long, User> users = new HashMap<>();
 

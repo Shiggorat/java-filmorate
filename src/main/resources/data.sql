6eade60ffe6706d3126bd3ddd9e75239bdd9806f
Gosling <popkornuh@gmail.com>
2025-03-30 17:01:30 +0300

    Задание 12 спринта

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,14 @@
 INSERT INTO genres (name)
-SELECT name FROM (VALUES
-    ('Комедия'),
-    ('Драма'),
-    ('Мультфильм'),
-    ('Триллер'),
-    ('Документальный'),
-    ('Боевик')
-) AS source (name)
-WHERE NOT EXISTS (SELECT 1 FROM genres WHERE genres.name = source.name);
+VALUES ('Комедия'),
+       ('Драма'),
+       ('Мультфильм'),
+       ('Триллер'),
+       ('Документальный'),
+       ('Боевик');
 
 INSERT INTO rating_mpa (name)
-SELECT name FROM (VALUES
-    ('G'),
-    ('PG'),
-    ('PG-13'),
-    ('R'),
-    ('NC-17')
-) AS source (name)
-WHERE NOT EXISTS (SELECT 1 FROM rating_mpa WHERE rating_mpa.name = source.name);
+VALUES ('G'),
+       ('PG'),
+       ('PG-13'),
+       ('R'),
+       ('NC-17');
